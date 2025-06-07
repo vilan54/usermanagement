92a0849d1fd2a209f7e1c966ce9f0396a9e06a02
Pablo Vilán Mancuello <pablo.vilan@udc.es>
2025-06-07 20:15:25 +0200

    Change the mockup data to permissions and roles for example

--- src/main/resources/db/migration/V2__add_data.sql
+++ src/main/resources/db/migration/V2__add_data.sql
@@ -1,23 +1,40 @@
 -- Insert default roles
 INSERT INTO roles (name, description) VALUES
 ('Admin', 'Administrator with full access'),
-('Editor', 'Editor with limited access'),
-('Viewer', 'User with view-only access');
+('User', 'Normal user with limited access');
 
 -- Insert default permissions
 INSERT INTO permissions (name, description) VALUES
-('READ', 'Permission to read data'),
-('WRITE', 'Permission to write data'),
-('DELETE', 'Permission to delete data'),
-('UPDATE', 'Permission to update data');
+
+-- Permissions for Admin
+('ADMIN_CREATE', 'Permission to create admin'),
+('ADMIN_READ', 'Permission to read admin data'),
+('ADMIN_WRITE', 'Permission to write admin data'),
+('ADMIN_DELETE', 'Permission to delete admin data'),
+('ADMIN_UPDATE', 'Permission to update admin data'),
+('ADMIN_VIEW_ALL', 'Permission to view all users'),
+('ADMIN_MANAGE_ROLES', 'Permission to manage user roles'),
+('ADMIN_MANAGE_PERMISSIONS', 'Permission to manage user permissions'),
+
+-- Permissions for User
+('USER_DELETE_SELF', 'Permission to delete user'),
+('USER_VIEW_SELF', 'Permission to view own user details'),
+('USER_UPDATE_SELF', 'Permission to update own user details'),
+('USER_CHANGE_PASSWORD_SELF', 'Permission to change own password');
+
 
 -- Assign permissions to roles
 INSERT INTO roles_permissions (id_role, id_permission) VALUES
-(1, 1), -- Admin can READ
-(1, 2), -- Admin can WRITE
-(1, 3), -- Admin can DELETE
-(1, 4), -- Admin can UPDATE
-(2, 1), -- Editor can READ
-(2, 2), -- Editor can WRITE
-(2, 4), -- Editor can UPDATE
-(3, 1); -- Viewer can only READ
+(1, 1), -- Admin can CREATE ADMIN
+(1, 2), -- Admin can READ ADMIN
+(1, 3), -- Admin can WRITE ADMIN
+(1, 4), -- Admin can DELETE ADMIN
+(1, 5), -- Admin can UPDATE ADMIN
+(1, 6), -- Admin can VIEW ALL USERS
+(1, 7), -- Admin can MANAGE ROLES
+(1, 8), -- Admin can MANAGE PERMISSIONS
+
+(2, 9), -- User can DELETE SELF
+(2, 10), -- User can VIEW SELF
+(2, 11), -- User can UPDATE SELF
+(2, 12); -- User can CHANGE PASSWORD SELF
