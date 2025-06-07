9633d82c95b9cb7bb67a4202f5ee7fdef10c2463
Pablo Vilán Mancuello <pablo.vilan@udc.es>
2025-06-07 17:05:03 +0200

    Change on name to follow convention

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -14,20 +14,20 @@ CREATE TABLE IF NOT EXISTS permissions (
 
 -- Relationship table between roles and permissions
 CREATE TABLE IF NOT EXISTS roles_permissions (
-    id_role BIGINT NOT NULL,
-    id_permission BIGINT NOT NULL,
-    PRIMARY KEY (id_role, id_permission),
-    FOREIGN KEY (id_role) REFERENCES roles(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_permission) REFERENCES permissions(id) ON DELETE CASCADE
+    role_id BIGINT NOT NULL,
+    permission_id BIGINT NOT NULL,
+    PRIMARY KEY (role_id, permission_id),
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (permission_id) REFERENCES permissions(id) ON DELETE CASCADE
 );
 
 -- Table of users
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    id_role BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_role) REFERENCES roles(id) ON DELETE CASCADE
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
 );
